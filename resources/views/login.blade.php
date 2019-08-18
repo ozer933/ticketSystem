6f29d5d3fb459f53c3b98ac5eb6955628128212f
İrfan Özer <ozer_93@outlook.com>
2019-08-19 00:21:08 +0300

    son hali

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title> Login | ArtıSubs Rapor </title>
+    <title> Login | TicketSystem </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
--- storage/framework/views/603a31d388595502766600e4839845664afc5c40.php
+++ storage/framework/views/603a31d388595502766600e4839845664afc5c40.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Ticket System</title>
+    <title>ArtiSubs Report</title>
 </head>
 
 <!-- Optional JavaScript -->
@@ -18,3 +18,4 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
+<?php /**PATH /mnt/c/www/ticketsystemlocal/resources/views/welcome.blade.php ENDPATH**/ ?>
